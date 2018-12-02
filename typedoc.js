90de2b5cab56296a2ad5c64bcef684f68294634e
Derrick Beining <derrick.beining@gmail.com>
2018-12-01 21:09:54 -0500

    build(typedoc): moved config to typedoc.js

---
+++ typedoc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  excludeExternals: true,
+  excludePrivate: true,
+  excludeProtected: true,
+  theme: "default",
+  name: "@dbeining/react-atom",
+  mode: "file",
+  out: "./docs",
+  includeDeclarations: true,
+  hideGenerator: true,
+  excludeNotExported: true
+};
