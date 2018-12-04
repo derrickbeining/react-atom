919559189a642bad36bac7787624da7c4858c908
Derrick Beining <derrick.beining@gmail.com>
2018-12-03 21:31:23 -0500

    refactor: use `isPOJO` in `isShallowEqual` for better POJO detection

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,13 @@
 /** @ignore */
+export function isPOJO(obj: unknown): obj is Record<string, unknown> {
+  if (typeof obj !== "object" || obj === null) return false;
+  const proto = Object.getPrototypeOf(obj);
+  return proto === Object.prototype || proto === null;
+}
+
+/** @ignore */
 export function isShallowEqual<A, B extends Record<string, unknown>, C extends A | B>(a: A, b: C): boolean {
-  if (a instanceof Object && b instanceof Object) {
+  if ([a, b].every(isPOJO) || [a, b].every(Array.isArray)) {
     if (Object.is(a, b)) return true;
     if (Object.keys(a).length !== Object.keys(b).length) return false;
     for (const k in a) if (!Object.is(a[k], b[k])) return false;
