db320e3f8271da6b4fd5c73ec1631a4316f8672b
Derrick Beining <derrick.beining@gmail.com>
2019-02-02 21:38:08 -0500

    feat: add `initialize` function to initialize with alt hooks
    This commit introduces a new way to initialize the library so that it is compatible with other
    versions/implementations of hooks. My use case for this at the moment is to be able to use
    pony/polyfills of hooks in react and react-native so that I can use hooks and react-atom in versions
    of react prior to the hooks release.

--- src/error-messages.ts
+++ src/error-messages.ts
@@ -3,9 +3,10 @@ export const calledUseAtomOutsideFunctionComponent =
   "useAtom can only be called inside the body of a function component";
 
 /** @ignore */
-export const calledUseAtomWithNonAtom =
-  "useAtom only accepts `react-atom` Atoms, but got:";
+export const calledUseAtomWithNonAtom = "useAtom only accepts `react-atom` Atoms, but got:";
 
 /** @ignore */
-export const calledDerefWithNonAtom =
-  "deref only accepts `react-atom` Atoms, but got:";
+export const calledDerefWithNonAtom = "deref only accepts `react-atom` Atoms, but got:";
+
+/** @ignore */
+export const multipleInstantiations = `Multiple instances of react-atom have been detected, which will lead to unexpected bugs in the useAtom custom hook. This usually means react-atom has been initialized with \`create(hooks)\` in addition to importing the default Atom, useAtom, etc. directly. To avoid this error, only use the implementation returned from \`create\`.`;
--- src/react-atom.ts
+++ src/react-atom.ts
@@ -1,2 +1,2 @@
-export { Atom, atom, deref, useAtom, swap, set } from "./react-atom-internal";
-export { AtomState } from "./internal-types";
+export { Atom, initialize as create, atom, deref, useAtom, swap, set } from "./react-atom-internal";
+export { AtomState, HookDependencies } from "./internal-types";
